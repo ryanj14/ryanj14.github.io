421fbecbfa289d510bdeeeff07b143cb5afc7f51
Ryan Joseph <josephmryan14@gmail.com>
2018-12-23 15:11:10 -0800

    Added css to index file

--- index.php
+++ index.php
@@ -3,8 +3,27 @@
   <head>
     <meta charset="UTF-8">
     <title>Ryan Joseph</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <script 
+      src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
+      crossorigin="anonymous">
+    </script>
+    <script 
+      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" 
+      integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" 
+      crossorigin="anonymous">
+    </script>
+    <script 
+      src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" 
+      integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" 
+      crossorigin="anonymous">
+    </script>
+    <!-- Personal CSS -->
+    <link rel="stylesheet" href="css/style.min.css">
   </head>
-  <body>
+  <body lang="en">
   <header>
       <h1>Ryan Joseph's Portfolio</h1>
   </header>
@@ -13,9 +32,24 @@
     <a href="https://nonionic-ohm.000webhostapp.com/">Hangman</a>
     <a href="https://project3900.000webhostapp.com/index.html">School Project</a>
     <nav>
+    <section>
+      <h2>Personal Work</h2>
+      <div class="projects">
+        <article>
+          <h3>Crowd Vote</h3>
+          <p></p>
+        </article>
+        <article>
+          <h3>Hangman</h3>
+          <p></p>
+        </article>
+        <article>
+          <h3>School Project</h3>
+          <p></p>
+        </article>
+      </div>
+    </section>
   </body>
-  <article>
-    <h2>Crowd Vote</h2>
-    <p></p>
-  </article>
+  <footer>
+  </footer>
 </html>

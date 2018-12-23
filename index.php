f4e68f62a9f9b183bfc1f9df8d45d3f7efa4a3ab
unknown <josephmryan14@gmail.com>
2018-12-23 14:49:13 -0800

    Added css folder and style files

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
-<?php
-    $jjj;
-    $jjj = "j";
-    print($jjj);
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Ryan Joseph</title>
+  </head>
+  <body>
+  <header>
+      <h1>Ryan Joseph's Portfolio</h1>
+  </header>
+    <nav>
+    <a href="https://crowdvote.000webhostapp.com/">Crowd Vote</a>
+    <a href="https://nonionic-ohm.000webhostapp.com/">Hangman</a>
+    <a href="https://project3900.000webhostapp.com/index.html">School Project</a>
+    <nav>
+  </body>
+  <article>
+    <h2>Crowd Vote</h2>
+    <p></p>
+  </article>
+</html>
